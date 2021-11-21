d7cd6103f3d4a6f6003591e132b5a411ba91225d
Ryan Epp <re17wj@brocku.ca>
2021-11-21 15:23:08 -0500

    Supports more feautres
    Excludes spaces, includes negatives

--- TTRPGRoller.py
+++ TTRPGRoller.py
@@ -1,38 +1,77 @@
 # Title: Table Top RPG Roller
-# Date: August 18, 2020
+# Date: November 21, 2021
 # Author: Ryan Epp
 # Description: Rolls virtual dice by text input and gives results
 
-import Dice
+import random
+
+def roll(sides, num):
+    sum = 0
+    for i in range(num):
+        sum = sum + random.randint(1,sides)
+    return sum
+
+class Roll():
+    def __init__(self, sides, num, isPos, isDice):
+        self.sides = sides
+        self.num = num
+        self.isPos = isPos
+        self.isDice = isDice
+    def eval(self):
+        final = 0
+        if self.isDice:
+            final = roll(self.sides, self.num)
+        else:
+            final = self.sides
+        if not self.isPos:
+            final = final * -1
+        return final
+
+def input_to_rolls(string):
+    roll_list = []
+
+    builder = ""
+    isPos = True
+    for c in string:
+        # Once we find 
+        if c == '+' or c == '-':
+            if builder != "":
+                roll_list.append(build_roll(builder, isPos))
+            builder = ""
+            if c == '+':
+                isPos = True
+            else:
+                isPos = False
+        else:
+            builder = builder + c
+    roll_list.append(build_roll(builder, isPos))
+
+    return roll_list
+
+def build_roll(builder, isPos):
+    split = builder.split('d')
+    if len(split) == 2:
+        numDice = int(split[0])
+        numSides = int(split[1])
+        return Roll(numSides, numDice, isPos, isDice=True)
+    else:
+        val = int(split[0])
+        return Roll(val, -1, isPos, False)
+    
 
 repeat = True
 
-print("TTRPG-Roller\n  Roll dice using XdXX (ex. 1d4+2d6)\n  Do not use spaces")
+print("TTRPG-Roller\n  Roll dice using XdXX (ex. 1d4+2d6-2)")
 
 while repeat: # keep rolling
     print("--------")
 
     # split dice types
-    dices = input().split("+")
+    rolls_to_process = input_to_rolls(input().replace(" ", ""))
 
     totalResult = 0
 
-    # have different dice types in list ["1d4", "1d10"]
-    for param in dices: # for every param
-        additive = 0
-        paramSplit = param.split("d") # split the numDice and the powerDice (ndp)
-        if(len(paramSplit) == 2): # check for dice or modifier
-            try:
-                numDice = int(paramSplit[0]) # number of dice to roll
-                powerDice = int(paramSplit[1]) # what kind of dice to roll (d4, d6, ect.)
-                # roll the dice and add them to the total road
-                additive = Dice.roll(powerDice, numDice)
-            except ValueError:
-                print("Not a number, try again.")
-                continue
-        else: # is a mod, add the value to the total
-            additive = int(paramSplit[0])
-
-        totalResult += additive 
+    for unit in rolls_to_process:
+        totalResult = totalResult + unit.eval()
 
     print("[" + str(totalResult) + "]")
